d275d6c5ade3d1928273af8ee9881208723fe197
Rajitha Ramanayake <rajitharamanayake@gmail.com>
2015-11-18 09:24:09 +0530

    clipsal commented

--- IoT1/routes/bulb.js
+++ IoT1/routes/bulb.js
@@ -12,17 +12,17 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 router.post('/on/:signal', function (req, res) {
-   
-    var bulb;
-    var ip = '000.00.00.00', mac = '000.00.000.00';
-    device.findOne({ 'id' : req.body.id , 'user' : req.body.user }, 'ip mac', function (err, device) {
+    
+    
+    var ip = '192.168.23.21', mac = '12.43.a1.34.54.65.78.54';
+    /*device.findOne({ 'id' : req.body.id , 'user' : req.body.user }, 'ip mac', function (err, device) {
         if (err) return handleError(err);
         ip = device.ip;
         mac = device.mac;
-    });
-
+    });       */
+    
     if (req.body.brand == 'philips_hue') {
-        bulb = new philips.philipsHue(ip, mac);
+         var bulb = new philips.philipsHue(ip, mac);
     }/* else if (req.brand == "clipsal") {
         var bulb = new clipsal.clipsalBulb(ip, mac);
     };*/
@@ -32,6 +32,6 @@ router.post('/on/:signal', function (req, res) {
         bulb.on();
     }
     res.send({ 'massege': 'done' });
-})
+});
 
 module.exports = router;
